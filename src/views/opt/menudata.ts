564e8f2d1e60cd91c8be0b71e542f9f3d0af0d6a
v_jeetang <v_jeetang@tencent.com>
2020-10-30 01:36:34 +0800

    提交

--- src/views/opt/menudata.ts
+++ src/views/opt/menudata.ts
@@ -4,7 +4,37 @@ const menu :MenuItem[]= [
 		name:'optbase1',
 		title:"基本优化",
 		icon:'ri-settings-6-fill'
-    }
+	},
+	{
+		name:'optbase2',
+		title:"渲染优化",
+		icon:'ri-strikethrough'
+	},
+	{
+		name:'optbase3',
+		title:"代码优化",
+		icon:'ri-strikethrough'
+	},
+	{
+		name:'optbase3',
+		title:"资源优化",
+		icon:'ri-strikethrough'
+	},
+	{
+		name:'optbase3',
+		title:"构建优化",
+		icon:'ri-strikethrough'
+	},
+	{
+		name:'optbase3',
+		title:"最佳实践",
+		icon:'ri-strikethrough'
+	},
+	{
+		name:'optbase3',
+		title:"其他补充",
+		icon:'ri-strikethrough'
+	},
 ]
 
 export default menu
